8f271da799852852b60a27926f651a2bce929121
Tendai <tendaimadzi@aol.com>
2021-09-30 10:32:00 +0200

    gen

---
+++ dist/_nuxt/static/1632990652/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632990652",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"80598989"}},globalRefs:{}}}(null,"\u002F"));
