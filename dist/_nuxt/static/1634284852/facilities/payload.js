a86340d5a346f830ab79fd29cfb6b9d00214b7c4
Takudzwa Madzikanda <taxbookair@Takudzwas-MacBook-Air.local>
2021-10-15 10:05:02 +0200

    second

---
+++ dist/_nuxt/static/1634284852/facilities/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/facilities", {data:[{}],fetch:{},mutations:void 0});
---
+++ dist/_nuxt/static/1634284852/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Ffacilities","\u002F","\u002Fclasses","\u002Fstaff","\u002Fcontact","\u002Ftrainers"]})
