da7a284119f1120ec06efabb06438674c71df4b4
TMadzi <tendaimadzi@aol.com>
2022-01-30 18:25:06 +0200

    fix homepage header

--- dist/_nuxt/static/1643554483/staff/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643554483",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstaff",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7d8e2b65"}},globalRefs:{},pinia:{}}}(null));
