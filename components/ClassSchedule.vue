c43a2ead73469eaf5685c9a900993e9316cc3975
TMadzi <tendaimadzi@aol.com>
2022-02-01 10:38:31 +0200

    add mobile schedule styles

--- components/ClassSchedule.vue
+++ components/ClassSchedule.vue
@@ -6,15 +6,15 @@
         {{ day.long }}
       </h2>
       <div v-for="eClass in classesStore.getDaysClasses(index)" :key="eClass.id" class=" text-gray-700 tracking-wide pt-2 pb-4 px-8 border-b border-gray-1  00 last:border-none">
-        <h4 class="mt-2">{{classesStore.getStartTimeBySessionId(eClass.session).hour+':'+classesStore.getStartTimeBySessionId(eClass.session).min+classesStore.getStartTimeBySessionId(eClass.session).suffix}}</h4>
-        <h3 class="text-2xl uppercase font-medium">
+        <h4 class="">{{classesStore.getStartTimeBySessionId(eClass.session).hour+':'+classesStore.getStartTimeBySessionId(eClass.session).min+classesStore.getStartTimeBySessionId(eClass.session).suffix}}</h4>
+        <h3 class="text-2xl uppercase font-bold">
           {{ eClass.class }}
         </h3>
         <h4 class="text-md">
           with {{ eClass.trainer }}
         </h4>
         <!-- <p class="text-sm mt-4">{{ eClass.desc }}</p> -->
-        <p class="text-sm mt-4">
+        <p class="text-sm mt-4 pb-3">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, magnam corrupti consectetur quibusdam porro nostrum ex! Nihil illum ea sapiente dolorum iste provident laborum maxime maiores iusto vel. Ducimus, sint.
         </p>
       </div>
--- dist/_nuxt/static/1643704412/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ffacilities\u002Fsteam-sauna","\u002Fterms-conditions","\u002Fstaff","\u002Fcontact","\u002Frates","\u002Ffacilities\u002Fweights","\u002Fclasses","\u002Ffacilities\u002Fcardio","\u002Ftrainers","\u002Ffacilities\u002Ffunctional"]})
